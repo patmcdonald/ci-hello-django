ab1d77cf49618f0edbc677bb7bb2473ac68f4b68
Pat McDonald <patmcdonald@me.com>
2022-07-26 17:43:41 +0000

    env.py defaults, move secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&%m@iyq+s(o+*i1!rq)!+l8vv46s@*p6!vb3k$5e&@(22)eu%a'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
